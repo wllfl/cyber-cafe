a3bced331b8bac7f2488641e5d5c149b35fee5b2
William <wllfl@ig.com.br>
2015-03-22 18:32:06 -0300

    Acerto do rodapé

--- include/inc_rodape.php
+++ include/inc_rodape.php
@@ -1,13 +1,27 @@
 <div id="rodape">
     <div class='container'>
 		<div class="row-fluid">
-			<div class="col-md-4 col-md-offset-4">
-		  		<span class="icon-facebook-circled iconeFacebook text-center"></span>
+			<div class="col-md-7 col-md-offset-3">
+		  		<span class="text-center">
+		  			<a href='' class='link-rodape'>Principal</a> |
+		  			<a href='' class='link-rodape'>Jogos</a> |
+		  			<a href='' class='link-rodape'>Conectividade</a> |
+		  			<a href='' class='link-rodape'>Serviços</a> |
+		  			<a href='' class='link-rodape'>Desenvolvimento</a> |
+		  			<a href='' class='link-rodape'>Contato</a>
+		  		</span>
 			</div>
-			<div class="col-md-3 col-md-offset-9">
+			<br/><br/>
+			<div class="col-md-4 col-md-offset-5">
+		  		<a href="#" class='link-icon'><i class="fa fa-facebook-square fa-3x"></i></a>
+		  		<a href="#" class='link-icon'><i class="fa fa-envelope-o fa-3x"></i></a>
+			</div>
+			<br/><br/><br/><br/>
+			<div class="col-md-3 col-md-offset-1">
 				<span class="text-center">Trabalhamos com PagSeguro</span>
 				<img src="img/pag-seguro.png" alt="PagSeguro">
 			</div>
+			<br/><br/><br/><br/>
 			<div class="col-md-4 col-md-offset-4">
 		  		<span class="location">Rua Monteiro Lobato, 381 - Vila Mayrink, </span>
 		  		<span class="location">Mairinque, SP | CEP: 18120-000</span>
