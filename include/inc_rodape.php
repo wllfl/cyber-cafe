6c6c1a84aab947e1d88cbcec15915db797e05572
William <wllfl@ig.com.br>
2015-03-22 12:28:33 -0300

    Acerto Inicial rodapé

--- include/inc_rodape.php
+++ include/inc_rodape.php
@@ -7,8 +7,12 @@
 			<div class="col-md-3 col-md-offset-9">
 				<span class="text-center">Trabalhamos com PagSeguro</span>
 				<img src="img/pag-seguro.png" alt="PagSeguro">
+			</div>
+			<div class="col-md-4 col-md-offset-4">
+		  		<span class="location">Rua Monteiro Lobato, 381 - Vila Mayrink, </span>
+		  		<span class="location">Mairinque, SP | CEP: 18120-000</span>
+				<h4 class="desenvolvido text-center">&copy; 2015 - Desenvolvido por: <strong>Cyber Café 24h</strong></h4>
 			</div>	  	
-			<h4 class="desenvolvido text-center">Desenvolvido por <strong>Cyber Café 24h</strong></h4>
 		</div>
 	</div>
 </div>
